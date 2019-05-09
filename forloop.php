917575c3995f3c16f2aab1b2807998d84d30b276
K.Kittipong <kittipong.kitc@gmail.com>
2019-05-09 10:22:00 +0700

    update forloop.php again

--- forloop.php
+++ forloop.php
@@ -9,4 +9,13 @@ for ($a = 1; $a <= 20; $a++)
     {
         echo $a . "Hello PHP<br>";
     }
+
+    if ($a % 2 == 1)
+    {
+        echo $a . "<font color = red>Hello PHP</font><br>";
+    }
+    else
+    {
+        echo $a . "Hello PHP<br>";
+    }
 }
